8914037aaeb3f1fd3c8a9d238876f3bc444429da
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-17 16:30:07 +0000

    implements ITicketed for Dodgems and Rollercoaster

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,24 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
+
+    private static double defaultPrice = 4.50;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+    }
+
+    public double defaultPrice() {
+        return defaultPrice;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if (visitor.getAge() < 12) {
+            return defaultPrice / 2;
+        }
+        return defaultPrice;
     }
 }
--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -4,11 +4,24 @@ import people.Visitor;
 
 public class RollerCoaster  extends Attraction {
 
+    private static double defaultPrice = 8.40;
+
     public RollerCoaster(String name, int rating) {
         super(name, rating);
     }
 
     public boolean isAllowedTo(Visitor visitor) {
         return visitor.getAge() > 12 && visitor.getHeight() > 145;
+    }
+
+    public double defaultPrice() {
+        return defaultPrice;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if (visitor.getHeight() > 200) {
+            return defaultPrice * 2;
+        }
+        return defaultPrice;
     }
 }
