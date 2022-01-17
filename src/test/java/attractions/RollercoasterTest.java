cc6e1fa9bc5604626d4eecd4cbf83453ba4ba47b
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-17 14:30:42 +0000

    adds security tests for Rollercoaster

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,12 +2,14 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor;
 
     @Before
     public void setUp() {
@@ -27,5 +29,29 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void canVisit() {
+        visitor = new Visitor(13, 146, 100);
+        assertTrue(rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void cannotVisitIfAgeIsLessThanThirteen() {
+        visitor = new Visitor(12, 146, 100);
+        assertFalse(rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void cannotVisitIfHeightIsLessThan145() {
+        visitor = new Visitor(35, 144, 100);
+        assertFalse(rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void cannotVisit() {
+        visitor = new Visitor(12, 144, 100);
+        assertFalse(rollerCoaster.isAllowedTo(visitor));
     }
 }
